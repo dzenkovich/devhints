155cd68ff7eb65280beb59b8d862ebb574d82ed5
Denis Zenkovich <denis.zenkovich@gmail.com>
2012-12-10 18:20:22 +0300

    Middle commit, completed script/tpl loader, started with views

--- ui/js/app.js
+++ ui/js/app.js
@@ -3,11 +3,104 @@
  *
  * @author Denis Zenkovich
  */
-$.getScript('models.js');
-$.getScript('views.js');
+
+/**
+ * Resources loader
+ *
+ * @type {Object} Singleton
+ */
+var Loader = (function(){
+    var _allScripts = []; //saves all loaded scripts just in case
+
+    return {
+        /**
+         * load the requested script
+         *
+         * @param url
+         * @param callback
+         * @param context
+         */
+        load: function(url, callback, context){
+            var i;
+            var done; //all scripts loaded callback
+            var getScript;
+            var scripts = []; //scripts loading in this batch
+            var inProgress = 0; //count of scripts in the progress of loading
+
+            if(!$.isArray(url)) url = [url];
+
+            done = function(){
+                //run the callback, pass all loaded scripts and let it decide if the failed or not
+                if(typeof callback == 'function'){
+                    if(typeof context == 'object'){
+                        callback.call(context, scripts);
+                    }
+                    else{
+                        callback(scripts);
+                    }
+                }
+            };
+
+            getScript = function(url){
+                var script;
+                var extension;
+                var dataType = 'script';
+
+                if(_allScripts[url]){
+                    return _allScripts[url];
+                }
+
+                extension = url.split('.').pop();
+                if(extension == 'html' || extension == 'htm'){
+                    dataType = 'html';
+                }
+
+                script = {
+                    url: url,
+                    loaded: false
+                };
+
+                $.ajax({
+                    url: url,
+                    dataType: dataType,
+                    complete: function(jqXHR, textStatus){
+                        inProgress--;
+                        script.isSuccess = (textStatus == 'success');
+                        script.loaded = true;
+                        if(dataType == 'html'){
+                            script.html = jqXHR.responseText;
+                        }
+                        if(inProgress === 0){
+                            if(typeof done == 'function') done();
+                        }
+                    }
+                });
+
+                inProgress++;
+                _allScripts[url] = script;
+                return script;
+            };
+
+            for(i = 0; i < url.length; i++){
+                scripts[url[i]] = getScript(url[i]);
+            }
+        }
+    };
+}());
 
 $(function(){
-    /**
-     *
-     */
-})
+    //start the application when all scripts are loaded
+    var start = function(){
+
+        var App = new AppView({
+            el: $('#header')
+        });
+
+    };
+
+    //load dependencies
+    Loader.load([
+        'ui/js/models.js',
+        'ui/js/views.js'
+    ], start);
+});
--- ui/js/views.js
+++ ui/js/views.js
@@ -5,17 +5,94 @@
  */
 
 /**
- * The main view of application: nabar, page creation/deletion and etc
+ * The main view of application: page navigation/creation/deletion and etc
  *
  * @type {*}
  */
-var App = Backbone.View.extend({
+var AppView = Backbone.View.extend({
 
+    _tpl: null, //saves tpl for re-renders
 
-    addPage: function(){
-
+    events: {
+        "click": "openPageAddModal"
     },
-    deletePage: function(){
+
+    initialize: function(){
+        Loader.load('ui/tpl/app.tpl.html', function(scripts){
+            var script = scripts['ui/tpl/app.tpl.html'];
+
+            if(script.isSuccess){
+                this._tpl = script.html;
+                this.render();
+            }
+        }, this);
+    },
+
+    render: function(){
+        this.$el.html(Mustache.render(this._tpl, {}));
+    },
+
+    openPageAddModal: function(){
 
     }
 })
+
+/**
+ * Generic modal window
+ *
+ * @type {*}
+ */
+var ModalView = Backbone.View.extend({
+    _tpl: null, //saves tpl for re-renders
+    _$content: null, //JQ modal content element
+    _$header: null, //JQ modal header element
+
+    _position: function(){
+
+    },
+
+    initialize: function(){
+        Loader.load('ui/tpl/modal.tpl.html', function(scripts){
+            var script = scripts['ui/tpl/modal.tpl.html'];
+
+            if(script.isSuccess){
+                this._tpl = script.html;
+            }
+        }, this);
+
+        //create page overlay if none exist yet, use it as a static property through all modals
+        if(!ModalView._$cover){
+            ModalView._$cover = $('<div class="cover"></div>').appendTo(document.body);
+        }
+    },
+
+    render: function(){
+        this.$el.html(Mustache.render(this._tpl, {}));
+        this._$content = this.$('.content');
+        this._$header = this.$('.header');
+
+    },
+
+    setTitle: function(title){
+        this.$('j-modal-title').html(title);
+    },
+
+    open: function(){
+
+    },
+
+    close: function(){
+
+    }
+});
+
+/**
+ * Add page modal interface, linked with AddPageModel
+ *
+ * @type {*}
+ */
+var AddPageModalView = ModalView.extend({
+
+
+
+});
